1f2cd5de231e7ce11a7b23acba435089ad804df2
juanferrer437 <juanferrer437@uoc.edu>
2022-11-18 16:46:19 +0100

    A

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import { tns } from 'tiny-slider';
+
+  var slider = tns({
+    container: '.my-slider',
+    autoplay: true,
+    mode:"gallery",
+    center:true,
+    controls:false,
+    nav:false
+  });
